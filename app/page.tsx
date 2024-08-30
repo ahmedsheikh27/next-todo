f6a26d27d40e5e1c81ecb827153b650f2add5b18
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2024-08-31 00:31:13 +0500

    remove bg

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TodoList from "@/components/TodoList";
 
 const Home: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex items-center justify-center ">
       <TodoList/> 
     </div>
   );
